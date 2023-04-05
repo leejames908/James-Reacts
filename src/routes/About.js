9d5a615db4daba1de72c6d967e3bf4e1a344dae9
FrootJam <lee.james908@gmail.com>
2023-04-05 09:08:19 -0400

    Update About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div>This is coming from the About js file, but the function this is literally written in is called about </div>
+    <div>This is coming from the About js file but the function this is literally written in is called about </div>
   )
 }
